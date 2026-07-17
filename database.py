298d9c041bd9491429095a86832962eac48e8d6a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-17 13:29:49 +0530

    setup connection with db

--- database.py
+++ database.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 DATABASE_URL="sqlite:///student.db"
 
 engine=create_engine(DATABASE_URL)
-SessionLocal=sessionmaker(bind=engine)
+SessionLocal=sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
